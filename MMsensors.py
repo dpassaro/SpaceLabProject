ed7f2ba52986a7eafe1d6bc8e6437ad4729abaf4
Daniele Passaro <d.passaro1@studenti.unipi.it>
2021-07-21 11:45:48 +0200

    Update MMsensors.py

--- MMsensors.py
+++ MMsensors.py
@@ -1,1 +1,20 @@
 
+import serial
+import time
+import os
+
+sensors_path = "/dev/tty_Arduino"
+sensors_bdrt = 115200
+
+sensors = serial.Serial(sensors_path, sensors_bdrt)
+
+fname = "data.log"
+
+log_file = open(fname, "a")
+  
+event = "ciao"
+
+while True:
+    event = sensors.readline().decode()
+    log_file.write(event)
+    
